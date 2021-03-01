88a770b450195e98bee24cfad9e9de2c7ac7f738
zs <>
2021-03-01 20:30:39 +0100

    Initial commit

---
+++ api/Entity.java
@@ -0,0 +1,10 @@
+package api;
+
+public interface Entity {
+
+    public static char delimiter = '\t';
+
+    public String values();
+    public String[] getFields();
+    public String[] getTableRow();
+}
---
+++ cli/Main.java
@@ -0,0 +1,17 @@
+package cli;
+
+import api.*;
+
+public class Main {
+
+    private static Dbmng dbm = new Dbmng("D:\\new.db");
+    public static void main(String[] args) {
+            MainMenu.Print();
+    }
+
+    public static Dbmng getDbm(){
+        return dbm;
+    }
+
+    
+}
